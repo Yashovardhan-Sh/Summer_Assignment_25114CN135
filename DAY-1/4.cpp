d5810ca94dc9bb1c39fe7a57ec7bd33faf651098
Yashovardhan-Sh <yashovardhansharma12@gmail.com>
2026-06-06 23:18:35 +0530

    Add files via upload

---
+++ DAY-1/4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n, count = 0;
+    cout<<"Enter a number : ";
+    cin>>n;
+    while(n>0){
+        n%10;
+        count++;
+        n/=10;
+    }
+    cout<<"Number of digits = "<<count;
+    return 0;
+
+}
