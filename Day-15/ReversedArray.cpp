c76ee535ff96f32506b85f98de2f41b0276fbafa
Yashovardhan-Sh <yashovardhansharma12@gmail.com>
2026-06-23 23:59:24 +0530

    Create ReversedArray.cpp

---
+++ Day-15/ReversedArray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, size);
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
