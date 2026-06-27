0b95e7f6b66081fdb34b722263aa02814f158b06
Yashovardhan-Sh <yashovardhansharma12@gmail.com>
2026-06-27 15:22:05 +0530

    Create RowWiseSumMatrices.cpp

---
+++ Day-20/RowWiseSumMatrices.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+
+    vector<vector<int>> mat(rows, vector<int>(cols));
+
+    cout << "Enter elements of the matrix:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> mat[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        int rowSum = 0;
+        for (int j = 0; j < cols; j++) {
+            rowSum += mat[i][j];
+        }
+        cout << "Sum of row " << i + 1 << ": " << rowSum << endl;
+    }
+
+    return 0;
+}
