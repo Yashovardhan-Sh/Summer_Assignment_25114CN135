10c8036efdfcca32b3ba367fafd2de358dcef311
Yashovardhan-Sh <yashovardhansharma12@gmail.com>
2026-06-27 15:13:58 +0530

    Create AddMatrices.cpp

---
+++ Day-19/AddMatrices.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+
+    vector<vector<int>> mat1(rows, vector<int>(cols));
+    vector<vector<int>> mat2(rows, vector<int>(cols));
+    vector<vector<int>> sum(rows, vector<int>(cols));
+
+    cout << "Enter elements of the first matrix:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> mat1[i][j];
+        }
+    }
+
+    cout << "Enter elements of the second matrix:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> mat2[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+
+    cout << "Resultant Matrix after addition:" << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
